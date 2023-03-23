fb633e5a7eaa28f5d73f672d9102418dbe672f39
pol.martin <pol.martin@mujin.co.jp>
2023-03-23 17:02:35 +0900

    Change path of bind

--- build/imgui.js
+++ build/imgui.js
@@ -7,7 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import * as Bind from "bind-imgui";
+import * as Bind from "./bind-imgui";
 export { Bind };
 let bind;
 export default function (value) {
